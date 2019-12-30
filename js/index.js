bf4d71daf96f218e89ea16bcb820575106a09a96
bstolarski <b.stolarski@hotmail.com>
2019-12-30 18:14:36 +0100

    Mobile view - in progress

---
+++ js/index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const button = document.querySelector(".page-nav-burger");
+    const menu = document.querySelector(".page-nav");
+
+    button.addEventListener("click", function(e) {
+        e.preventDefault();
+        menu.classList.toggle("show");
+
+    });
+});
